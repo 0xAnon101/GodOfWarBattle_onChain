e04383ef3bc4cd88b46b05cce91490226378db6a
Raju <rajugautam45@gmail.com>
2022-01-31 14:05:04 +0530

    add args to verify bytecode in etherscan

---
+++ scripts/arguments.js
@@ -0,0 +1,20 @@
+module.exports = [
+  [
+    "Angel Pepe",
+    "Devil Pepe",
+    "Doctor Pepe",
+    "Hulk Pepe",
+    "Robot Pepe",
+    "Transformer Pepe",
+  ], // Names
+  [
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/angel_pepe.png", // Images
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/devil_pepe.png",
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/doctor_pepe.png",
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/hulk_pepe.png",
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/robot_pepe.png",
+    "https://bafybeif4juhqio5534y6hvhb7flrcgl73mocze4xvf2tkppwaajeppd3ei.ipfs.dweb.link/transformer_pepe.png",
+  ],
+  [100, 200, 50, 150, 125, 300], // HP values
+  [90, 100, 60, 100, 90, 150], // Attack damage values
+];
